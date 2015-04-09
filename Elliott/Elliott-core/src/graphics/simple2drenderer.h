1c6af383b244eae031d49ee43d3b4d9a74eb5d04
Félix Poitras <felixpoitras@bell.net>
2015-04-09 14:57:16 -0400

    Simple 2D Renderer

---
+++ Elliott/Elliott-core/src/graphics/simple2drenderer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <deque>
+#include "renderer2D.h"
+
+namespace elliott { namespace graphics {
+
+	class Simple2DRenderer : public Renderer2D
+	{
+	private:
+		std::deque<const Renderable2D*> m_RenderQueue;
+	public:
+		virtual void submit(const Renderable2D* renderable) override;
+		virtual void flush() override;
+	};
+
+} }
